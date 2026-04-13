598c15db41e16b2aa865dc8efd6b465e8b99071a
viduu24 <126349679+viduu24@users.noreply.github.com>
2026-04-13 12:32:42 -0400

    Update page_missingness.py

--- Menstrual_cycle_dashboard/app_pages/page_missingness.py
+++ Menstrual_cycle_dashboard/app_pages/page_missingness.py
@@ -7,7 +7,7 @@ def show():
     
     dataset = st.selectbox(
         "Select a dataset to view:",
-        ["Kaggle", "Hormones+symptoms", "heart rate and hormones symptoms merged"]
+        ["Kaggle", "Hormones+symptoms"]
     )
     
     base_path = os.path.dirname(os.path.dirname(__file__))
@@ -17,13 +17,11 @@ def show():
         img = Image.open(img_path)
         st.image(img, caption="Missing Values Heatmap - Kaggle Dataset", use_column_width=True)
         
-    elif dataset == "Hormones+symptoms":
+    else:
         img_path = os.path.join(base_path, "missing_values_heatmap1.png")
         img = Image.open(img_path)
         st.image(img, caption="Missing Values Heatmap - Hormones+Symptoms Dataset", use_column_width=True)
         
         
-    else:
-        img_path = os.path.join(base_path, "hr_missingvalues.png")
-        img = Image.open(img_path)
-        st.image(img, caption="Missing Values Heatmap - Merged HR Dataset", use_column_width=True)
+    
+       
